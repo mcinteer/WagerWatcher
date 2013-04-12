8a950c9faee58159aee664033626049f0dda7812
mcinteer <ryan.mcinteer@gmail.com>
2013-04-13 08:39:47 +1200

    XML stucture for Odds

---
+++ WagerWatcher/Model/Odds/XMLBetTypeFromOdds.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace WagerWatcher.Model.Odds
+{
+    [Serializable, XmlRoot("bet_type")]
+    public class XMLBetTypeFromOdds
+    {
+        [XmlAttribute("comingled")]
+        public string Comingled { get; set; }
+
+        [XmlAttribute("comingled_info")]
+        public string ComingledInfo { get; set; }
+
+        [XmlAttribute("comingled_brand")]
+        public string ComingledBrand { get; set; }
+    }
+}
---
+++ WagerWatcher/Model/Odds/XMLRaceFromOdds.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace WagerWatcher.Model.Odds
+{
+    [Serializable, XmlRoot("race")]
+    public class XMLRaceFromOdds
+    {
+        [XmlAttribute("number")]
+        public string Number { get; set; }
+
+        [XmlAttribute("winpool")]
+        public string WinPool { get; set; }
+
+        [XmlAttribute("plcpool")]
+        public string PlacePool { get; set; }
+
+        [XmlAttribute("qlapool")]
+        public string QuinellaPool { get; set; }
+
+        [XmlAttribute("tfapool")]
+        public string TrifectaPool { get; set; }
+
+        [XmlElement("entries")]
+        public XMLEntriesRootFromOdds EntriesRoot { get; set; }
+
+        [XmlElement("pools")]
+        public XMLPoolsRootFromOdds PoolsRoot { get; set; }
+        
+    }
+}
