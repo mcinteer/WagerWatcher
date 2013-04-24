3e7e189ba2799755f9278a1ed35b7866269c4349
mcinteer <ryan.mcinteer@gmail.com>
2013-04-25 11:29:35 +1200

    1

--- WagerWatcher/Model/Schedule/XMLMeetingsRootFromSchedule.cs
+++ WagerWatcher/Model/Schedule/XMLMeetingsRootFromSchedule.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Xml.Serialization;
-using Mindscape.LightSpeed;
 using WagerWatcher.Model.Schedule;
 
 namespace WagerWatcher.Model
--- WagerWatcher/Repositories/MeetingRepository.cs
+++ WagerWatcher/Repositories/MeetingRepository.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Mindscape.LightSpeed;
 using NHibernate.Criterion;
 
 namespace WagerWatcher.Repositories
